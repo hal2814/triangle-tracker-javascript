cd21d725261c38fda9a188745def225135053ae9
Michael Brooks and Nate McGregor <michael@>
2017-10-10 14:27:43 -0700

    fix bugs, refactor

--- js/triangle.js
+++ js/triangle.js
@@ -1,12 +1,33 @@
 function Triangle(side1,side2,side3) {
-  this.side1 = side1;
-  this.side2 = side2;
-  this.side3 = side3;
+  this.side1 = parseInt(side1);
+  this.side2 = parseInt(side2);
+  this.side3 = parseInt(side3);
 }
+
+Triangle.prototype.sideSame = function () {
+  if(this.side1 === this.side2){
+    if(this.side1 + this.side2 > this.side3){
+
+      return true;
+    }
+  }
+  if(this.side1 === this.side3){
+    if(this.side1 + this.side3 > this.side2){
+
+      return true;
+    }
+  }
+  if(this.side2 === this.side3){
+    if(this.side2 + this.side3 > this.side1){
+
+      return true;
+    }
+  }
+};
 
 // has 2 equal sides
 Triangle.prototype.isosceles = function () {
-  if((this.side1 === this.side2) || (this.side3 === this.side2) || (this.side1 === this.side3)) {
+  if(this.sideSame()) {
     return true;
   }
 };
@@ -27,14 +48,15 @@ Triangle.prototype.scalene = function () {
 
 //checks if it is a triangle
 Triangle.prototype.isTriangle = function () {
-  if (((this.side1 + this.side2) <= this.side3) || ((this.side2 + this.side3) <= this.side1) || ((this.side3 + this.side1) <= this.side2)) {
-    return 0;
-  }else {
+  if (((this.side1 + this.side2) >= this.side3) || ((this.side2 + this.side3) >= this.side1) || ((this.side3 + this.side1) >= this.side2)) {
     if (this.equilateral()) {
       return 2;
     }
     else if (this.isosceles()) {
       return 3;
+    }
+    else if (((this.side1 + this.side2) <= this.side3) || ((this.side2 + this.side3) <= this.side1) || ((this.side3 + this.side1) <= this.side2)) {
+      return 0;
     }
     else if (this.scalene()) {
       return 1;
